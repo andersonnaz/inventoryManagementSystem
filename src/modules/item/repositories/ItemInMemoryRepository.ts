004bc264bd39fb00d628ffa3abebe2c25603e81f
andersonnaz <anderson_1004@hotmail.com>
2023-05-27 18:05:50 -0300

    feat: :sparkles: add save method

--- src/modules/item/repositories/ItemInMemoryRepository.ts
+++ src/modules/item/repositories/ItemInMemoryRepository.ts
@@ -4,6 +4,10 @@ import { ItemRepository } from "./ItemRepository";
 class ItemInMemoryRepository implements ItemRepository {
     private _database: Item[] = [];
 
+    save(item: Item) {
+        this._database.push(item);
+    }
+
 }
 
 export default new ItemInMemoryRepository();
