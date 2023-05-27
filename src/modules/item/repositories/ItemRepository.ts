c9ba0cf82173359c67a3fa2dd7e72413e5cec11c
andersonnaz <anderson_1004@hotmail.com>
2023-05-27 19:35:26 -0300

    fix: :bug: add param on method list by category in interface Item Repository

--- src/modules/item/repositories/ItemRepository.ts
+++ src/modules/item/repositories/ItemRepository.ts
@@ -3,5 +3,5 @@ import { Item } from "../entities/Item";
 
 export interface ItemRepository extends Repository<Item> {
     listBySupplier(): Item[];
-    listByCategory(): Item[];
+    listByCategory(category: string): Item[];
 }
