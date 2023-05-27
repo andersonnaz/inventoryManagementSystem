28bb4c51a28478da123fc3519a22892827c182ae
andersonnaz <anderson_1004@hotmail.com>
2023-05-27 15:44:36 -0300

    feat: :sparkles: add validate method in Supplier entity and put in create

--- src/modules/supplier/entities/Supplier.ts
+++ src/modules/supplier/entities/Supplier.ts
@@ -12,7 +12,24 @@ export class Supplier {
     }
 
     public static create(name: string, cnpj: string, address: string, phone: string): Supplier {
+        this.validate(name, cnpj, address, phone);
         const supplier = new Supplier(name, cnpj, address, phone);
         return supplier;
+    }
+
+    private static validate(name: string, cnpj: string, address: string, phone: string): boolean {
+        if(!name){
+            throw new Error('name invalid!');
+        }
+        if(!cnpj){
+            throw new Error('cnpj invalid');
+        }
+        if(!address){
+            throw new Error('address invalid');
+        }
+        if(!phone){
+            throw new Error('phone invalid');
+        }
+        return true;
     }
 }
