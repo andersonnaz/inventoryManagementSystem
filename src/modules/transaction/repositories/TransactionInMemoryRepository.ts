f7a0020f97a901fc464537202a7f1ffac6faee9e
andersonnaz <anderson_1004@hotmail.com>
2023-05-28 11:51:11 -0300

    feat: :sparkles: add save method

--- src/modules/transaction/repositories/TransactionInMemoryRepository.ts
+++ src/modules/transaction/repositories/TransactionInMemoryRepository.ts
@@ -1,8 +1,12 @@
+import { Transaction } from "../entities/Transaction";
 import { TransactionRepository } from "./TransactionRepository";
 
 class TransactionInMemoryRepository implements TransactionRepository {
     private _database: Transaction[] = [];
 
+    save(transaction: Transaction) {
+        this._database.push(transaction);
+    }
 }
 
 export default new TransactionInMemoryRepository();
