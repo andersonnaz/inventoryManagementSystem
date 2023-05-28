82a8871a586b73405255d0b141627e4cfcc89fa9
andersonnaz <anderson_1004@hotmail.com>
2023-05-27 21:26:04 -0300

    feat: :sparkles: add save method

--- src/modules/customer/repositories/CustomerInMemoryRepository.ts
+++ src/modules/customer/repositories/CustomerInMemoryRepository.ts
@@ -4,6 +4,10 @@ import { Customer } from "../entities/Customer";
 class CustomerInMemoryRepository implements Repository<Customer> {
     private _database: Customer[] = [];
 
+    save(customer: Customer) {
+        this._database.push(customer);
+    }
+
 }
 
 export default new CustomerInMemoryRepository();
