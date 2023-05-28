3db51db873831b8cd26f6c641bebed6b132ec9c6
andersonnaz <anderson_1004@hotmail.com>
2023-05-28 12:10:05 -0300

    feat: :sparkles: add findOne method

--- src/modules/transaction/repositories/TransactionInMemoryRepository.ts
+++ src/modules/transaction/repositories/TransactionInMemoryRepository.ts
@@ -1,3 +1,5 @@
+import { Customer } from "../../customer/entities/Customer";
+import { Supplier } from "../../supplier/entities/Supplier";
 import { Transaction } from "../entities/Transaction";
 import { TransactionRepository } from "./TransactionRepository";
 
@@ -10,6 +12,17 @@ class TransactionInMemoryRepository implements TransactionRepository {
 
     list(): Transaction[] {
         return this._database;
+    }
+
+    findOne(param: string): Transaction | undefined {
+        const transaction = this._database.find((transaction) => {
+            if(transaction.entity instanceof Customer){return transaction.entity.cpf === param};
+            if(transaction.entity instanceof Supplier){return transaction.entity.cnpj === param};
+        })
+        if(!transaction){
+            throw new Error('transaction not found!');
+        }
+        return transaction;
     }
 }
 
