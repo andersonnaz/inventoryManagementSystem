9695e198d659704b60846e4cd7505337d409ca41
andersonnaz <anderson_1004@hotmail.com>
2023-05-28 10:50:27 -0300

    feat: :sparkles: add save method

--- src/modules/supplier/repositories/SupplierInMemoryRepository.ts
+++ src/modules/supplier/repositories/SupplierInMemoryRepository.ts
@@ -3,6 +3,10 @@ import { Supplier } from "../entities/Supplier";
 
 class SupplierInMemoryRepository implements Repository<Supplier>{
     private _database: Supplier[] = [];
+
+    save(supplier: Supplier) {
+        this._database.push(supplier);
+    }
 }
 
 export default new SupplierInMemoryRepository();
