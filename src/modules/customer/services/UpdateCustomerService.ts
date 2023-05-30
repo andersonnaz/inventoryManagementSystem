98ad67934aed9fad2dd589052575b113e25d2965
andersonnaz <anderson_1004@hotmail.com>
2023-05-30 20:14:29 -0300

    feat: :sparkles: add service update customer

---
+++ src/modules/customer/services/UpdateCustomerService.ts
@@ -0,0 +1,15 @@
+import { Repository } from "../../../shared/database/Repository";
+import { Customer } from "../entities/Customer";
+
+export class UpdateCustomerService {
+    private readonly _customerRepository: Repository<Customer>;
+
+    constructor(customerRepository: Repository<Customer>){
+        this._customerRepository = customerRepository;
+    }
+
+    async execute(cpf: string, updateCustomer: Customer): Promise<Customer> {
+        const customer = await this._customerRepository.update(cpf, updateCustomer);
+        return customer;
+    }
+}
