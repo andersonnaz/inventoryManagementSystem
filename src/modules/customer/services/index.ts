f2c3a964df24d3be7228deb8590d6ad5041b33f9
andersonnaz <anderson_1004@hotmail.com>
2023-05-30 20:15:56 -0300

    feat: :sparkles: add index file that concentrates all service customers

---
+++ src/modules/customer/services/index.ts
@@ -0,0 +1,5 @@
+export * from './CreateCustomerService';
+export * from './DeleteCustomerService';
+export * from './FindCustomerService';
+export * from './ListCustomerService';
+export * from './UpdateCustomerService';
