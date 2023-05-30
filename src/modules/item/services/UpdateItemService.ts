1f5bce67df7ad0239cdb8b1c03c136e771646533
andersonnaz <anderson_1004@hotmail.com>
2023-05-30 14:18:47 -0300

    feat: :sparkles: add service update item

---
+++ src/modules/item/services/UpdateItemService.ts
@@ -0,0 +1,15 @@
+import { Item } from "../entities/Item";
+import { ItemRepository } from "../repositories/ItemRepository"
+
+export class UpdateItemService {
+    private readonly _itemRepository: ItemRepository;
+
+    constructor(itemRepository: ItemRepository){
+        this._itemRepository = itemRepository;
+    };
+
+    async execute(param: string, updateItem: Item): Promise<Item> {
+        const item = await this._itemRepository.update(param, updateItem);
+        return item;
+    }
+}
