564453f59faa2016be32a9e97ed54d3f3e014ccd
andersonnaz <anderson_1004@hotmail.com>
2023-05-30 21:12:14 -0300

    feat: :sparkles: add index file that concentrates all service suppliers

---
+++ src/modules/supplier/services/index.ts
@@ -0,0 +1,5 @@
+export * from './CreateSupplierService';
+export * from './DeleteSupplierService';
+export * from './FindSupplierService';
+export * from './ListSupplierService';
+export * from './UpdateSupplierService';
