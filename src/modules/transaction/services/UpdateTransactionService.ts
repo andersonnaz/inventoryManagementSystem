b12792ab17f7090121ce44d6861ecd636cd1af57
andersonnaz <anderson_1004@hotmail.com>
2023-06-01 03:51:18 -0300

    feat: :sparkles: add service update transaction

---
+++ src/modules/transaction/services/UpdateTransactionService.ts
@@ -0,0 +1,15 @@
+import { Transaction } from "../entities/Transaction";
+import { TransactionRepository } from "../repositories/TransactionRepository";
+
+export class UpdateTransactionService {
+    private readonly _transactionRepository: TransactionRepository;
+
+    constructor(transactionRepository: TransactionRepository){
+        this._transactionRepository = transactionRepository;
+    }
+
+    async execute(id: string, updateTransaction: Transaction): Promise<Transaction>{
+        const transaction = await this._transactionRepository.update(id, updateTransaction);
+        return transaction;
+    }
+}
