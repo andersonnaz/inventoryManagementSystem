93a33ab85fc43dfb5497b0e076c52ebf3b4e248c
andersonnaz <anderson_1004@hotmail.com>
2023-06-02 22:00:16 -0300

    feat: :sparkles: add delete customer controller

---
+++ src/modules/customer/controllers/DeleteCustomerController.ts
@@ -0,0 +1,21 @@
+import CustomerInMemoryRepository from "../repositories/CustomerInMemoryRepository";
+import { Request, Response } from "express";
+import { DeleteCustomerService } from "../services";
+
+export class DeleteCustomerController {
+    private readonly _deleteCustomerService: DeleteCustomerService;
+
+    constructor(){
+        this._deleteCustomerService = new DeleteCustomerService(CustomerInMemoryRepository);
+    }
+
+    delete = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const {id} = request.params;
+            const result = await this._deleteCustomerService.execute(id);
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
