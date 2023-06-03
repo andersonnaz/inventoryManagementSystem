3a8a19d55e038f9c2d8c6774034e7d84a4e01d5c
andersonnaz <anderson_1004@hotmail.com>
2023-06-02 22:01:04 -0300

    feat: :sparkles: add update customer controller

---
+++ src/modules/customer/controllers/UpdateCustomerController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import CustomerInMemoryRepository from "../repositories/CustomerInMemoryRepository";
+import { UpdateCustomerService } from "../services";
+import { UpdateCustomerDTO } from "../dto/UpdateCustomerDTO";
+
+export class UpdateCustomerController {
+    private readonly _updateCustomerService: UpdateCustomerService;
+
+    constructor(){
+        this._updateCustomerService = new UpdateCustomerService(CustomerInMemoryRepository);
+    }
+
+    update = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const {id} = request.params;
+            const updateCustomerDTO: UpdateCustomerDTO = request.body;
+            const result = await this._updateCustomerService.execute(id, updateCustomerDTO);
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
