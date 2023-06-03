7e91a2aaeda25fabdc1ab2eb0d8dd6ddc3399cc9
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 16:57:27 -0300

    fix: :bug: change argument in list by supplier

--- src/modules/item/repositories/ItemRepository.ts
+++ src/modules/item/repositories/ItemRepository.ts
@@ -2,6 +2,6 @@ import { Repository } from "../../../shared/database/Repository";
 import { Item } from "../entities/Item";
 
 export interface ItemRepository extends Repository<Item> {
-    listBySupplier(): Item[];
+    listBySupplier(supplier: string): Item[];
     listByCategory(category: string): Item[];
 }
