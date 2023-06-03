0f4ce1ebdde4516921df01a2c6007fd2cfce83af
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 17:01:18 -0300

    feat: :sparkles: add find item controller

---
+++ src/modules/item/controllers/FindItemController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import ItemInMemoryRepository from "../repositories/ItemInMemoryRepository";
+import { FindItemService } from "../services";
+
+export class FindItemController {
+    private readonly _findItemService: FindItemService;
+
+    constructor(){
+        this._findItemService = new FindItemService(ItemInMemoryRepository);
+    }
+
+    find = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const {id} = request.params;
+            const result = await this._findItemService.execute(id);
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
