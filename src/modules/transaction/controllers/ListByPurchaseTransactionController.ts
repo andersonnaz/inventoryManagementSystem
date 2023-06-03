c957450a4abb41cb0a1bb12a06b624d8a7314dd8
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 18:16:37 -0300

    feat: :sparkles: add list by purchase transaction controller

---
+++ src/modules/transaction/controllers/ListByPurchaseTransactionController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import TransactionInMemoryRepository from '../repositories/TransactionInMemoryRepository';
+import { ListByPurchaseTransactionService } from '../services';
+
+export class ListByPurchaseTransactionController {
+    private readonly _listByPurchaseTransactionService: ListByPurchaseTransactionService;
+
+    constructor(){
+        this._listByPurchaseTransactionService = new ListByPurchaseTransactionService(TransactionInMemoryRepository);
+    }
+
+    listByPurchase = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const result = await this._listByPurchaseTransactionService.execute();
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
