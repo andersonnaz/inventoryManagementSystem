02e9df35e2727c1ffd86f50930a166d9e9b97fd7
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 15:21:22 -0300

    feat: :sparkles: add create supplier controller

---
+++ src/modules/supplier/controllers/CreateSupplierController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { CreateSupplierService } from '../services';
+import SupplierInMemoryRepository from '../repositories/SupplierInMemoryRepository';
+import { CreateSupplierDTO } from '../dto/CreateSupplierDTO';
+
+export class CreateSupplierController {
+    private readonly _createSupplierService: CreateSupplierService;
+
+    constructor(){
+        this._createSupplierService = new CreateSupplierService(SupplierInMemoryRepository);
+    }
+
+    create = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const createSupplierDTO: CreateSupplierDTO = request.body;
+            const result = await this._createSupplierService.execute(createSupplierDTO);
+            return response.status(201).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
