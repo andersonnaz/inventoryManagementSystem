f5707f9e2dee22c8c896052c4a00e15faf218aaf
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 17:04:54 -0300

    feat: :sparkles: add index file that concentrates all controller items

---
+++ src/modules/item/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './CreateItemController';
+export * from './DeleteItemController';
+export * from './FindItemController';
+export * from './ListItemByCategoryController';
+export * from './ListItemBySupplierController';
+export * from './ListItemController';
+export * from './UpdateItemController';
