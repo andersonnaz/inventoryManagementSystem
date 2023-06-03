4610fc9da593d0c619b88ebbe9a149ee25173ec0
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 17:03:59 -0300

    feat: :sparkles: add list all items controller

---
+++ src/modules/item/controllers/ListItemController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import ItemInMemoryRepository from "../repositories/ItemInMemoryRepository";
+import { ListItemService } from "../services";
+
+export class ListItemController {
+    private readonly _listItemService: ListItemService;
+
+    constructor(){
+        this._listItemService = new ListItemService(ItemInMemoryRepository);
+    }
+
+    list = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const result = await this._listItemService.execute();
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
