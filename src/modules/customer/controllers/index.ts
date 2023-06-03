09091ea0f3b3045968458aa16cad6c680db651b2
andersonnaz <anderson_1004@hotmail.com>
2023-06-02 22:02:00 -0300

    feat: :sparkles: add index file that concentrates all controller customers

---
+++ src/modules/customer/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './CreateCustomerController';
+export * from './DeleteCustomerController';
+export * from './FindCustomerRepository';
+export * from './ListCustomerController';
+export * from './UpdateCustomerController';
