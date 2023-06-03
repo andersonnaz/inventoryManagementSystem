8489e24c53ceace7d345ee95f3d0d9b740946e49
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 18:15:17 -0300

    feat: :sparkles: add find transaction controller

---
+++ src/modules/transaction/controllers/FindTransactionController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import TransactionInMemoryRepository from '../repositories/TransactionInMemoryRepository';
+import { FindTransactionService } from '../services/FindTransactionService';
+
+export class FindTransactionController {
+    private readonly _findTransactionService: FindTransactionService;
+
+    constructor(){
+        this._findTransactionService = new FindTransactionService(TransactionInMemoryRepository);
+    }
+
+    find = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const {id} = request.params;
+            const result = await this._findTransactionService.execute(id);
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
