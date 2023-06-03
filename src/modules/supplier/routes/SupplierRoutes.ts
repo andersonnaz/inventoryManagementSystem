9f71fbe28493f73b79aa7d15a9be495e1be6096e
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 19:38:37 -0300

    feat: :sparkles: add supplier routes

---
+++ src/modules/supplier/routes/SupplierRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+    CreateSupplierController,
+    DeleteSupplierController,
+    FindSupplierController,
+    ListSupplierController,
+    UpdateSupplierController
+} from '../controllers';
+
+const supplierRouter = Router();
+const createSupplierController = new CreateSupplierController();
+const deleteSupplierController = new DeleteSupplierController();
+const findSupplierController = new FindSupplierController();
+const listSupplierController = new ListSupplierController();
+const updateSupplierController = new UpdateSupplierController();
+
+supplierRouter.get('/', listSupplierController.list);
+supplierRouter.post('/', createSupplierController.create);
+supplierRouter.get('/:id', findSupplierController.find);
+supplierRouter.delete('/:id', deleteSupplierController.delete);
+supplierRouter.put('/', updateSupplierController.update);
+
+export default supplierRouter;
+
