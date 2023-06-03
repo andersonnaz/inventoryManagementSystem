ff639e0874976471a6f771d497b72cbf4760d897
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 16:59:15 -0300

    fix: :bug: change argument type in create item service

--- src/modules/item/services/CreateItemService.ts
+++ src/modules/item/services/CreateItemService.ts
@@ -2,6 +2,7 @@ import { ItemRepository } from "../repositories/ItemRepository";
 import { Item } from '../../item/entities/Item';
 import { Supplier } from "../../supplier/entities/Supplier";
 import { Repository } from "../../../shared/database/Repository";
+import { CreateItemDTO } from "../dto/CreateItemDTO";
 
 export class CreateItemService {
     private readonly _itemRepository: ItemRepository;
@@ -12,12 +13,12 @@ export class CreateItemService {
         this._supplierRepository = supplierRepository;
     }
 
-    async execute(name: string, category: string, supplierName: string): Promise<Item> {
-        const supplier = await this._supplierRepository.findOne(supplierName);
-        if(!supplier) {
+    async execute({name, category, supplier}: CreateItemDTO): Promise<Item> {
+        const supplierFound = await this._supplierRepository.findOne(supplier);
+        if(!supplierFound) {
             throw new Error('supplier not found!');
         }
-        const item = Item.create(name, category, supplier.name);
+        const item = Item.create(name, category, supplierFound.name);
         await this._itemRepository.save(item);
         return item;
     };
