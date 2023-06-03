c2ffceaaec235318cbf75bdaf5ad69d4466438da
andersonnaz <anderson_1004@hotmail.com>
2023-06-02 21:57:08 -0300

    fix: :bug: change type of argument in execute method

--- src/modules/customer/services/CreateCustomerService.ts
+++ src/modules/customer/services/CreateCustomerService.ts
@@ -1,4 +1,5 @@
 import { Repository } from "../../../shared/database/Repository";
+import { CreateCustomerDTO } from "../dto/CreateCustomerDTO";
 import { Customer } from '../entities/Customer';
 
 export class CreateCustomerService {
@@ -8,7 +9,7 @@ export class CreateCustomerService {
         this._customerRepository = customerRepository;
     }
 
-    async execute(name: string, cpf: string, address: string, phone: string): Promise<Customer> {
+    async execute({name, cpf, address, phone}: CreateCustomerDTO): Promise<Customer> {
         const customer = Customer.create(name, cpf, address, phone);
         await this._customerRepository.save(customer);
         return customer;
