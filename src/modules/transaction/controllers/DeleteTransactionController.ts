e6fb8e475a69006ec891c7440beac2ddfde13187
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 18:14:53 -0300

    feat: :sparkles: add delete transaction controller

---
+++ src/modules/transaction/controllers/DeleteTransactionController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import TransactionInMemoryRepository from "../repositories/TransactionInMemoryRepository";
+import { DeleteTransactionService } from "../services/DeleteTransactionService";
+
+export class DeleteTransactionController {
+    private readonly _deleteTransactionService: DeleteTransactionService;
+
+    constructor(){
+        this._deleteTransactionService = new DeleteTransactionService(TransactionInMemoryRepository);
+    }
+
+    delete = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const {id} = request.params;
+            const result = await this._deleteTransactionService.execute(id);
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
