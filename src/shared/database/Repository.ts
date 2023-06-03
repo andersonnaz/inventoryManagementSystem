15f329c053fba5908f68212ca7b987a6d0dad5bf
andersonnaz <anderson_1004@hotmail.com>
2023-06-02 21:53:58 -0300

    fix: :bug: change update method, param now is partial T

--- src/shared/database/Repository.ts
+++ src/shared/database/Repository.ts
@@ -2,6 +2,6 @@ export interface Repository<T> {
     save(param: T): void;
     list(): T[];
     findOne(param: string): T | undefined;
-    update(id: string, param: T): T;
+    update(id: string, param: Partial<T>): T;
     delete(id: string): void;
 }
