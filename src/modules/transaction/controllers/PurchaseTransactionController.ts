ef0528ffcc557ef4cd4be41560d4bec692439885
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 18:14:31 -0300

    feat: :sparkles: add purchase transaction controller

---
+++ src/modules/transaction/controllers/PurchaseTransactionController.ts
@@ -0,0 +1,28 @@
+import ItemInMemoryRepository from '../../item/repositories/ItemInMemoryRepository';
+import TransactionInMemoryRepository from '../repositories/TransactionInMemoryRepository';
+import { PurchaseTransactionService } from '../services/PurchaseTransactionService';
+import SupplierInMemoryRepository from '../../supplier/repositories/SupplierInMemoryRepository';
+import { Request, Response } from 'express';
+
+export class PurchaseTransactionController {
+    private readonly _purchaseTransactionService: PurchaseTransactionService;
+
+    constructor(){
+        this._purchaseTransactionService = new PurchaseTransactionService(
+            TransactionInMemoryRepository,
+            SupplierInMemoryRepository,
+            ItemInMemoryRepository
+        );
+    }
+
+    purchase = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const {id} = request.params;
+            const {items} = request.body;
+            const result = await this._purchaseTransactionService.execute(id, items);
+            return response.status(201).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
