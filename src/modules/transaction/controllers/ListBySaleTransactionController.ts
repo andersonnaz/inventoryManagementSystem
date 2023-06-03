77ca8a687fb5a8f7adf5c8ba238fbb3e71e626e1
andersonnaz <anderson_1004@hotmail.com>
2023-06-03 18:16:17 -0300

    feat: :sparkles: add list by sale transaction controller

---
+++ src/modules/transaction/controllers/ListBySaleTransactionController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import TransactionInMemoryRepository from "../repositories/TransactionInMemoryRepository";
+import { ListBySaleTransactionService } from "../services";
+
+export class ListBySaleTransactionController {
+    private readonly _listBySaleTransactionService: ListBySaleTransactionService;
+
+    constructor(){
+        this._listBySaleTransactionService = new ListBySaleTransactionService(TransactionInMemoryRepository);
+    }
+
+    listBySale = async(request: Request, response: Response): Promise<Response> => {
+        try {
+            const result = await this._listBySaleTransactionService.execute();
+            return response.status(200).json(result);
+        } catch (error) {
+            return response.status(404).json({message: error.message});
+        }
+    }
+}
