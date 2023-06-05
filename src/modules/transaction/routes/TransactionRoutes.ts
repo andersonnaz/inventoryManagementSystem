74fc573704914f1e4ebbf305337b1db590e9fee2
andersonnaz <anderson_1004@hotmail.com>
2023-06-05 10:24:39 -0300

    fix: :bug: fix transaction routes list by sale and list by purchase

--- src/modules/transaction/routes/TransactionRoutes.ts
+++ src/modules/transaction/routes/TransactionRoutes.ts
@@ -21,12 +21,12 @@ const listByPurchaseTransaction = new ListByPurchaseTransactionController();
 const listBySaleTransaction = new ListBySaleTransactionController();
 
 transactionsRouter.get('/', listTransactionController.list);
-transactionsRouter.post('/sale', saleTransactionController.sale);
-transactionsRouter.post('/purchase', purchaseTransactionController.purchase);
+transactionsRouter.get('/sale', listBySaleTransaction.listBySale);
+transactionsRouter.get('/purchase', listByPurchaseTransaction.listByPurchase);
+transactionsRouter.post('/sale/:id', saleTransactionController.sale);
+transactionsRouter.post('/purchase/:id', purchaseTransactionController.purchase);
 transactionsRouter.get('/:id', findTransactionController.find);
 transactionsRouter.delete('/:id', deleteTransactionController.delete);
 transactionsRouter.put('/:id', updateTransactionController.update);
-transactionsRouter.get('/sale', listBySaleTransaction.listBySale);
-transactionsRouter.get('/purchase', listByPurchaseTransaction.listByPurchase);
 
 export default transactionsRouter;
