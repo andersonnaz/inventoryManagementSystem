1973410dcb37aa90e199d90b61a47226c315b5a7
andersonnaz <anderson_1004@hotmail.com>
2023-06-05 10:23:00 -0300

    fix: :bug: fix item routes list by category and by supplier

--- src/modules/item/routes/ItemRoutes.ts
+++ src/modules/item/routes/ItemRoutes.ts
@@ -23,7 +23,7 @@ itemsRouter.post('/', createItemController.create);
 itemsRouter.get('/:id', findItemController.find);
 itemsRouter.delete('/:id', deleteItemController.delete);
 itemsRouter.put('/:id', updateItemController.update);
-itemsRouter.get('/:category', listItemByCategoryController.listItemByCategory);
-itemsRouter.get('/:supplier', listItemBySupplierController.listItemBySupplier);
+itemsRouter.get('/category/:category', listItemByCategoryController.listItemByCategory);
+itemsRouter.get('/supplier/:supplier', listItemBySupplierController.listItemBySupplier);
 
 export default itemsRouter;
