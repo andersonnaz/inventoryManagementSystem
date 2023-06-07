231f75a86f9920ede6ea57db40aac41322ebdca7
andersonnaz <anderson_1004@hotmail.com>
2023-06-07 00:30:22 -0300

    refactor: :recycle: change data received of request body in create customer controller

--- src/modules/customer/controllers/CreateCustomerController.ts
+++ src/modules/customer/controllers/CreateCustomerController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { CreateCustomerService } from '../services';
-import { CreateCustomerDTO } from '../dto/CreateCustomerDTO';
 
 export class CreateCustomerController {
     private readonly _createCustomerService: CreateCustomerService;
@@ -11,8 +10,8 @@ export class CreateCustomerController {
 
     create = async(request: Request, response: Response): Promise<Response> => {
         try {
-            const createCustomerDTO: CreateCustomerDTO = request.body;
-            const result = await this._createCustomerService.execute(createCustomerDTO);
+            const { name, cpf, address, phone } = request.body;
+            const result = await this._createCustomerService.execute({name, cpf, address, phone});
             return response.status(201).json(result);
         } catch (error) {
             return response.status(404).json(error);          
