843234acbed8bc88fa2e6e361ccbc391b08a1276
andersonnaz <anderson_1004@hotmail.com>
2023-06-07 00:33:22 -0300

    refactor: :recycle: change type of argument in update customer service

--- src/modules/customer/services/UpdateCustomerService.ts
+++ src/modules/customer/services/UpdateCustomerService.ts
@@ -1,5 +1,6 @@
 import { Repository } from "../../../shared/database/Repository";
 import { Customer } from "../entities/Customer";
+import { UpdateCustomerDTO } from '../dto/UpdateCustomerDTO';
 
 export class UpdateCustomerService {
     private readonly _customerRepository: Repository<Customer>;
@@ -8,7 +9,7 @@ export class UpdateCustomerService {
         this._customerRepository = customerRepository;
     }
 
-    async execute(cpf: string, updateCustomer: Partial<Customer>): Promise<Customer> {
+    async execute(cpf: string, updateCustomer: UpdateCustomerDTO): Promise<Customer> {
         const customer = await this._customerRepository.update(cpf, updateCustomer);
         return customer;
     }
