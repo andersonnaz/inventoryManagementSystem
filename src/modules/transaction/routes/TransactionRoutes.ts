e68523bc0bc20ecdda162a88724cdf1e9269dcbd
andersonnaz <anderson_1004@hotmail.com>
2023-06-09 10:46:01 -0300

    refactor: :recycle: refact index file controller

--- src/modules/transaction/routes/TransactionRoutes.ts
+++ src/modules/transaction/routes/TransactionRoutes.ts
@@ -1,28 +1,20 @@
 import { Router } from "express";
 import {
-    DeleteTransactionController,
-    FindTransactionController,
-    ListByPurchaseTransactionController,
-    ListBySaleTransactionController,
-    ListTransactionController,
-    PurchaseTransactionController,
-    SaleTransactionController,
-    UpdateTransactionController
+    deleteTransactionController,
+    findTransactionController,
+    listByPurchaseTransactionController,
+    listBySaleTransactionController,
+    listTransactionController,
+    purchaseTransactionController,
+    saleTransactionController,
+    updateTransactionController
 } from '../controllers';
 
 const transactionsRouter = Router();
-const saleTransactionController = new SaleTransactionController();
-const purchaseTransactionController = new PurchaseTransactionController();
-const deleteTransactionController = new DeleteTransactionController();
-const findTransactionController = new FindTransactionController();
-const listTransactionController = new ListTransactionController();
-const updateTransactionController = new UpdateTransactionController();
-const listByPurchaseTransaction = new ListByPurchaseTransactionController();
-const listBySaleTransaction = new ListBySaleTransactionController();
 
 transactionsRouter.get('/', listTransactionController.list);
-transactionsRouter.get('/sale', listBySaleTransaction.listBySale);
-transactionsRouter.get('/purchase', listByPurchaseTransaction.listByPurchase);
+transactionsRouter.get('/sale', listBySaleTransactionController.listBySale);
+transactionsRouter.get('/purchase', listByPurchaseTransactionController.listByPurchase);
 transactionsRouter.post('/sale/:id', saleTransactionController.sale);
 transactionsRouter.post('/purchase/:id', purchaseTransactionController.purchase);
 transactionsRouter.get('/:id', findTransactionController.find);
