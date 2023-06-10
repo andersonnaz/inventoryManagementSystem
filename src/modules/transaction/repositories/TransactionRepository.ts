5cc1f3342984ef4671429f1bf14421eb0e22aa99
andersonnaz <anderson_1004@hotmail.com>
2023-06-10 00:04:09 -0300

    refactor: :recycle: refact interface transaction repository

--- src/modules/transaction/repositories/TransactionRepository.ts
+++ src/modules/transaction/repositories/TransactionRepository.ts
@@ -1,7 +1,15 @@
-import { Repository } from "../../../shared/database/Repository";
+import { Deletable } from "../../../shared/database/Repository/Deletable";
+import { Listable } from "../../../shared/database/Repository/Listable";
+import { Findable } from "../../../shared/database/Repository/Findable";
+import { Saveable } from "../../../shared/database/Repository/Saveable";
 import { Transaction } from "../entities/Transaction";
 
-export interface TransactionRepository extends Repository<Transaction> {
+export interface TransactionRepository extends
+    Saveable<Transaction>,
+    Deletable,
+    Findable<Transaction>,
+    Listable<Transaction>{
+
     listByCustomer(): Transaction[];
     listBySupplier(): Transaction[];
 }
