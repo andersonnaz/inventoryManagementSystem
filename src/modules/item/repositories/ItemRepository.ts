c68c6a9b25066475dc981a7a1bae3dfa176ecb34
andersonnaz <anderson_1004@hotmail.com>
2023-06-09 23:46:54 -0300

    refactor: :recycle: refact interface item repository

--- src/modules/item/repositories/ItemRepository.ts
+++ src/modules/item/repositories/ItemRepository.ts
@@ -1,7 +1,17 @@
-import { Repository } from "../../../shared/database/Repository";
+import { Deletable } from "../../../shared/database/Repository/Deletable";
+import { Listable } from "../../../shared/database/Repository/Listable";
+import { Updatable } from "../../../shared/database/Repository/Updatable";
+import { Findable } from "../../../shared/database/Repository/Findable";
+import { Saveable } from "../../../shared/database/Repository/Saveable";
 import { Item } from "../entities/Item";
 
-export interface ItemRepository extends Repository<Item> {
+export interface ItemRepository extends
+    Saveable<Item>,
+    Deletable,
+    Findable<Item>,
+    Listable<Item>,
+    Updatable<Item> {
+
     listBySupplier(supplier: string): Item[];
     listByCategory(category: string): Item[];
 }
